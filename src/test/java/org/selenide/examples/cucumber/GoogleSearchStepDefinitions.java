a59e2377399c8af2a82ecb2797e0f50d247ed54d
Stanislav Ivanov <sivanov@pentalog.com>
2020-04-04 23:37:00 +0300

    Added project with first files

---
+++ src/test/java/org/selenide/examples/cucumber/GoogleSearchStepDefinitions.java
@@ -0,0 +1,36 @@
+package org.selenide.examples.cucumber;
+
+import com.codeborne.selenide.Configuration;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThanOrEqual;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GoogleSearchStepDefinitions {
+
+  @Given("an open browser with google.com")
+  public void openGoogleSearch() {
+    Configuration.reportsFolder = "target/surefire-reports";
+    open("https://google.com/ncr");
+  }
+
+  @When("a keyword {string} is entered in input field")
+  public void enterKeyword(String keyword) {
+    $(By.name("q")).val(keyword).pressEnter();
+  }
+
+  @Then("at least top {int} matches should be shown")
+  public void topTenMatchesShouldBeShown(int resultsCount) {
+    $$("#res .g").shouldHave(sizeGreaterThanOrEqual(resultsCount));
+  }
+
+  @Then("the first one should contain {string}")
+  public void the_first_one_should_contain_keyword(String expectedText) {
+    $("#res .g").shouldHave(text(expectedText));
+  }
+}
+
